2024ad9448165b6ffca0b644044f37cae5ad0a2d
loretobravo <lbrasot@gmail.com>
2022-01-04 18:04:09 -0300

    Snapshot showed in console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {firestore} from "./Firebase"
 import "./index.css";
 import './App.css';
 import Home from "./Pages/Home";
 
 
 function App() {
+
   return (
     
       <Home/>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import {firestore} from "../Firebase"
 
 const Home = () => {
+
+    useEffect(()=>{
+        firestore
+          .collection("tweets")
+          .get()
+          .then(snapshot =>{
+            snapshot.forEach(doc=>{
+              console.log(doc.data());
+            })
+          })
+      })
     return (
         <div>
             <h1>
